d2f50bd58a8d57b4323ed33d266c3953d00d8aaa
Cory Fail <me@coryfail.com>
2022-01-20 22:24:59 -0500

    Fixed route issue

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import { Header } from './Components/Layout/Header/Header'
 
@@ -10,11 +10,11 @@ export const Layout = () => {
                 <Header />
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 relative">
                     <div className="max-w-3xl mx-auto">
-                        <BrowserRouter>
+                        <HashRouter>
                             <Routes>
                                 <Route path="/" element={<Home />} />
                             </Routes>
-                        </BrowserRouter>
+                        </HashRouter>
                     </div>
                 </div>
             </div>
